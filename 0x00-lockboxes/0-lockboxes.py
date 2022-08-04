836f845349cb0c0fda7672753978a63ca8464ab2
rayenhdr93 <hedrirayen69@gmail.com>
2022-08-04 12:28:12 +0100

    R 0-

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    for j in range(1, len(boxes) - 1):
+        x = 0
+        i = 0
+        for box in boxes:
+            if (j in box) and (j != i):
+                x = 1
+            i = i + 1
+        if x == 0:
+            return(False)
+    return(True)
