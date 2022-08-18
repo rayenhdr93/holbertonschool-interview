dd7d2c4ee81027c8f32db324ce28216b9fd48929
rayenhdr93 <hedrirayen69@gmail.com>
2022-08-18 23:10:21 +0100

    binary tree

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - hi
+ * @parent: hi
+ * @value: hi
+ * Return: hi
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+	if (!new)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
+}
