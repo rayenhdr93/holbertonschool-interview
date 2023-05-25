e2ebd58a55ac25128ed6403c7bb7a40548f4eddc
rayenhdr93 <hedrirayen69@gmail.com>
2023-05-25 17:56:21 +0100

    0-validate_utf8.py

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -3,19 +3,20 @@
 
 
 def validUTF8(data):
-        count = 0
+    """ UTF-8 Validation validUTF8(data) """
+    count = 0
 
-        for byte in data:
-            byte |= 256
-            if (byte >> 3 == 0b1_11111 or (byte >> 6 == 0b1_10)^(count>0)):
-                return False
-            if (byte >> 5 == 0b1_110):
-                count = 1
-            elif (byte >> 4 == 0b1_1110):
-                count = 2
-            elif (byte >> 4 == 0b1_1111):
-                count = 3
-            elif (byte >> 6 == 0b1_10):
-                count-= 1
+    for byte in data:
+        byte |= 256
+        if (byte >> 3 == 0b1_11111 or (byte >> 6 == 0b1_10)^(count>0)):
+            return False
+        if (byte >> 5 == 0b1_110):
+            count = 1
+        elif (byte >> 4 == 0b1_1110):
+            count = 2
+        elif (byte >> 4 == 0b1_1111):
+            count = 3
+        elif (byte >> 6 == 0b1_10):
+            count-= 1
 
-        return (count == 0)
+    return (count == 0)
