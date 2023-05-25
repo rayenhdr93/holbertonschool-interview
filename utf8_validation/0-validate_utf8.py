d7a2ed4bc2bc720c07758afb652fd58b6af76f8d
rayenhdr93 <hedrirayen69@gmail.com>
2023-05-25 18:09:25 +0100

    0-validate_utf8.py

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -16,16 +16,6 @@ def validUTF8(data):
 set represents a valid UTF-8 encoding
     """
     count = 0
-    for byte in data:
-        byte |= 256
-        if (byte >> 3 == 0b1_11111 or (byte >> 6 == 0b1_10)^(count>0)):
-            return False
-        if (byte >> 5 == 0b1_110):
-            count = 1
-        elif (byte >> 4 == 0b1_1110):
-            count = 2
-        elif (byte >> 4 == 0b1_1111):
-            count = 3
-        elif (byte >> 6 == 0b1_10):
-            count-= 1
+    if data > 10:
+        count = 7 
     return (count == 0)
