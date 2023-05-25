574313dfed927b93dc66c3ccd90c6d9ab83343fc
rayenhdr93 <hedrirayen69@gmail.com>
2023-05-25 18:22:08 +0100

    0-validate_utf8.py

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -13,7 +13,7 @@ need to handle the 8 least significant bits of each integer"""
 def validUTF8(data):
 	"""
     method that determines if a given data
-set represents a valid UTF-8 encoding
+set represents a valid UTF-8 encoding ..
     """	
 	count = 0
 	for byte in data:
