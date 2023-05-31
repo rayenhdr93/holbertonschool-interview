6b8afff9964f6a445fdb752fa28e83da363f8db1
rayenhdr93 <hedrirayen69@gmail.com>
2023-05-31 16:29:27 +0100

    0-slide_line.c

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -12,9 +12,13 @@ int slide_line(int *line, size_t size, int direction){
 	int x;
 	int newline[32];
 
+
+	for (i = 0; i < 32; i++)
+		newline[i] = 0;
 	if (direction == SLIDE_LEFT){
 		x = line[0];
 		j = 0;
+		newline[j] = x;
 		for (i = 1; i < k; i++){
 			/*printf("i = %ld,j = %ld, x = %d\n", i, j, x);*/
 			if (line[i] == x && line[i] != 0){
@@ -22,44 +26,42 @@ int slide_line(int *line, size_t size, int direction){
 				x = 0;
 				j++;
 			}
+			else if (line[i] != 0 && 0 != newline[j]){
+				j++;
+				x = line[i];
+				newline[j] = x;
+			}
 			else if (line[i] != 0){
 				x = line[i];
 				newline[j] = x;
 			}
 		}
-		if (newline[j] == x)
-				j++;
-		for (i = j; i < k; i++){
-			newline[i] = 0;
-		}
-		for (i = 0; i < k; i++){
-		line[i] = newline[i];
-	}
 	}
 	else if (direction == SLIDE_RIGHT){
 		x = line[size - 1];
 		j = size - 1;
+		newline[j] = x;
 		for (i = size - 2; i >= 0; i--){
 			if (line[i] == x && line[i] != 0){
 				newline[j] = x * 2;
 				x = 0;
 				j--;
 			}
+			else if (line[i] != 0 && 0 != newline[j]){
+				j--;
+				x = line[i];
+				newline[j] = x;
+			}
 			else if (line[i] != 0){
 				x = line[i];
 				newline[j] = x;
 			}
 		}
-		if (newline[j] == x)
-				j--;
-		for (i = j; i >= 0; i--){
-			newline[i] = 0;
-		}
-		for (i = size - 1; i >= 0; i--){
-		line[i] = newline[i];
-	}
 	}
 	else
 		return (0);
+	for (i = 0; i < k; i++){
+		line[i] = newline[i];
+	}
 	return (1);
 }
