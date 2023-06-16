b5aeacc930d66ba7cb4ed5ae546a751badee1c9a
rayenhdr93 <hedrirayen69@gmail.com>
2023-06-16 21:27:12 +0100

    0-nqueens.py

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -12,7 +12,7 @@ def nqueens(n: int):
     def is_valid(row, col):
         for i in range(row):
             if columns[i] == col or columns[i] - i == \
-                col - row or columns[i] + i == col + row:
+            col - row or columns[i] + i == col + row:
                 return False
         return True
 
