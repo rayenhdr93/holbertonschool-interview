f78782a8b997ce853500ca93f74df1443c3df3ed
rayenhdr93 <hedrirayen69@gmail.com>
2023-06-16 21:22:53 +0100

    0-nqueens.py

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -8,7 +8,7 @@ import sys
 def nqueens(n: int):
     result = []
     columns = [0] * n
-    
+
     def is_valid(row, col):
         for i in range(row):
             if columns[i] == col or columns[i] - i == col - row or columns[i] + i == col + row:
@@ -29,12 +29,15 @@ def nqueens(n: int):
         print(board)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("Usage: nqueens N")
-    if not sys.argv[1].isdigit():
-        print("N must be a number")
-        exit(1)
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        exit(1)    
-    nqueens(int(sys.argv[1]))
+    elif len(sys.argv) != 2:
+        print("Usage: nqueens N")
+    if len(sys.argv) == 2:
+        if not sys.argv[1].isdigit():
+            print("N must be a number")
+            exit(1)
+        if int(sys.argv[1]) < 4:
+            print("N must be at least 4")
+            exit(1)
+        nqueens(int(sys.argv[1]))
