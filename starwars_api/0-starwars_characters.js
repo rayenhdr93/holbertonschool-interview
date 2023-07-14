51d7b94150d75f71cc724bc38152800db8eb3a32
rayenhdr93 <hedrirayen69@gmail.com>
2023-07-14 07:50:01 +0100

    0-starwars_characters.js

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
 request(url, async function (error, response, body) {
-    if (error) console.log(error);
+  if (error) console.log(error);
   for (const character of JSON.parse(body).characters) {
     await new Promise((resolve, reject) => {
       request(character, function (error, response, body) {
